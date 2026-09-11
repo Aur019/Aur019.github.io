44040b9192ce1a6b61775e32f75240fb409fc1f7
Aur019 <316274847+Aur019@users.noreply.github.com>
2026-09-11 16:06:45 +0000

    Falta CSS

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -32,17 +32,32 @@ $(function () {
 
     // TODO 2 - Create Platforms
     createPlatform(130, 690, 270, 15, "green")
-
-
+    createPlatform(350, 600, 270, 15, "Red")
+    createPlatform(640, 478, 130, 130, "Yellow")
+    createPlatform(790, 600, 270, 15, "blue")
+    createPlatform(1060, 690, 270, 15, "pink")
+    createPlatform(350, 400, 200, 30, "purple")
+    createPlatform(850, 400, 200, 30, "black")
+    createPlatform(130, 290, 150, 25, "lime")
+    createPlatform(130, 210, 30, 10, "lime")
+    createPlatform(295, 100, 330, 25, "blue")
+    createPlatform(1100, 300, 15, 15, "hotpink")
+    createPlatform(1350, 300, 15, 15, "pink")
+    createPlatform(130, 150, 15, 150, "lime")
 
     // TODO 3 - Create Collectables
 
+    createCollectable("diamond", 1280, 610, .3, 1.2)
+    createCollectable("steve", 1210, 210)
+    createCollectable("max", 590, 40)
 
 
-    
     // TODO 4 - Create Cannons
 
-
+    createCannon("left", 20, 890)
+    createCannon("bottom", 740, 1500)
+    createCannon("bottom", 560, 1500)
+    createCannon("top", 1290, 1500)
     
     
     //////////////////////////////////
