fcff5db7567225fb345a1e51eea5a3939faa3431
Aur019 <316274847+Aur019@users.noreply.github.com>
2026-09-23 15:51:19 +0000

    Making it pretty

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,37 +27,38 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    toggleGrid();
+
 
 
     // TODO 2 - Create Platforms
-    createPlatform(130, 690, 270, 15, "green")
-    createPlatform(350, 600, 270, 15, "Red")
-    createPlatform(640, 478, 130, 130, "Yellow")
-    createPlatform(790, 600, 270, 15, "blue")
-    createPlatform(1060, 690, 270, 15, "pink")
-    createPlatform(350, 400, 200, 30, "purple")
-    createPlatform(850, 400, 200, 30, "black")
-    createPlatform(130, 290, 150, 25, "lime")
-    createPlatform(130, 210, 30, 10, "lime")
-    createPlatform(295, 100, 330, 25, "blue")
-    createPlatform(1100, 300, 15, 15, "hotpink")
-    createPlatform(1350, 300, 15, 15, "pink")
-    createPlatform(130, 150, 15, 150, "lime")
+    createPlatform(130, 690, 270, 15, "Black");
+    createPlatform(350, 600, 270, 15, "black");
+    createPlatform(640, 478, 130, 130, "#E5CE39");
+    createPlatform(790, 600, 270, 15, "black");
+    createPlatform(1060, 690, 270, 15, "black");
+    createPlatform(350, 400, 200, 30, "black");
+    createPlatform(850, 400, 150, 30, "#E5CE39", 850, 850, 0, 290, 430,2);  
+    createPlatform(130, 290, 150, 25, "black");
+    createPlatform(130, 210, 30, 10, "#E5CE39" );
+    createPlatform(295, 100, 330, 25, "black");
+    createPlatform(1100, 320, 15, 15, "black");
+    createPlatform(1350, 320, 15, 15, "#E5CE39");
+    createPlatform(130, 150, 15, 150, "black");
+    createBadPlatform(1100, 335, 265, 18, "#B90000");
 
     // TODO 3 - Create Collectables
 
-    createCollectable("diamond", 1280, 610, .3, 1.2)
-    createCollectable("steve", 1210, 210)
-    createCollectable("max", 590, 40)
+    createCollectable("diamond", 1280, 610, .3, 1.2);
+    createCollectable("steve", 1210, 190);
+    createCollectable("max", 590, 40);
 
 
     // TODO 4 - Create Cannons
 
-    createCannon("left", 20, 890)
-    createCannon("bottom", 740, 1500)
-    createCannon("bottom", 560, 1500)
-    createCannon("top", 1290, 1500)
+    createCannon("left", 20, 890);
+    createCannon("bottom", 740, 1500);
+    createCannon("bottom", 560, 1500);
+    createCannon("top", 1290, 1500);
     
     
     //////////////////////////////////
